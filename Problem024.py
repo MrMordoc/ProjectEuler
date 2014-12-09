ad8d90a4bee2a7aaabcca0b46ffb9157964cbbee
Ron Fenolio <mordoc@gmail.com>
2014-12-09 09:14:00 +0900

    initial commit

---
+++ Problem024.py
@@ -0,0 +1,15 @@
+"""finds the one millionth permutation in lexographic order of the digits 0 through 9"""
+
+import itertools
+perms = itertools.permutations(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+
+count = 0
+for item in perms:
+	if count == 999999:
+		num = ""
+		for digit in item:
+			num += digit
+		print int(num)
+		break
+	else:
+		count += 1
